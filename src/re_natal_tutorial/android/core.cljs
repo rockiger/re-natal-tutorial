a28b6438efe36436f4c87529f342b9b11e426577
Marco Laspe <marco@rockiger.com>
2016-08-17 22:44:41 +0200

    fix naming

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -6,14 +6,14 @@
 (def app-registry (.-AppRegistry ReactNative))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn lots-of-styles []
+(defn fixed-dimensions-basics []
   [view
     [view {:style {:width 50 :height 50 :background-color :powderblue}}]
     [view {:style {:width 100 :height 100 :background-color :skyblue}}]
     [view {:style {:width 150 :height 150 :background-color :steelblue}}]])
 
 (defn app-root []
-  [lots-of-styles])
+  [fixed-dimensions-basics])
 
 (defn init []
       (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
