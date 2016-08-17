6e0c1a1361acbf5053913a0423f67954a2b5906e
Marco Laspe <marco@rockiger.com>
2016-08-17 22:48:51 +0200

    Height and Width: Flex Dimensions

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -6,14 +6,14 @@
 (def app-registry (.-AppRegistry ReactNative))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn fixed-dimensions-basics []
-  [view
-    [view {:style {:width 50 :height 50 :background-color :powderblue}}]
-    [view {:style {:width 100 :height 100 :background-color :skyblue}}]
-    [view {:style {:width 150 :height 150 :background-color :steelblue}}]])
+(defn flex-dimensions-basics []
+  [view {:style {:flex 1}}
+    [view {:style {:flex 1 :background-color :powderblue}}]
+    [view {:style {:flex 2 :background-color :skyblue}}]
+    [view {:style {:flex 3 :background-color :steelblue}}]])
 
 (defn app-root []
-  [fixed-dimensions-basics])
+  [flex-dimensions-basics])
 
 (defn init []
       (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
