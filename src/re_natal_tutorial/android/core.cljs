be00b235246e7cc728656cc665dd4c23b692d129
Marco Laspe <marco@rockiger.com>
2016-08-18 19:02:06 +0200

    Using a ListView

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -4,50 +4,31 @@
 (def ReactNative (js/require "react-native"))
 
 (def app-registry (.-AppRegistry ReactNative))
-(def scroll-view (r/adapt-react-class (.-ScrollView ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
+(def list-view (r/adapt-react-class (.-ListView ReactNative)))
+(def data-source (.-DataSource (.-ListView ReactNative)))
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(def state (r/atom "")) ;; we are using global state
-(def favicon (js/require "./images/cljs.png"))
+(defn- convert-to-array [vec]
+  "Vector -> JavaScript Array
+  consumes a Vector and produces an array, it is needed for
+  the list-view, because it only accepts an real JavaScript Array."
+  (let [arr #js[]] (doseq [x vec] (.push arr x)) arr))
 
-(defn i-scrolled-down []
-  [scroll-view
-    [text {:style {:font-size 96}} "Scroll me plz"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "If you like"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Scrolling down"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "What's the best"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Framework around"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Re-Natal"]])
+(def ds (new data-source #js{:rowHasChanged #(not= %1 %2)}))
+
+(def state (.cloneWithRows ds ;; we are using global state
+             (convert-to-array ["John" "Joel" "James" "Jimmy" "Jackson"
+                                "Jillian" "Julie" "Devin"])))
+
+(defn list-view-basics []
+  [view {:style {:padding-top 22}}
+    [list-view {:dataSource state ;for whatever reason :data-source doesn't work
+                ;; Turn the vector into a React element
+                :render-row #(r/as-element [text %])}]])
 
 (defn app-root []
-  [i-scrolled-down])
+  [list-view-basics])
 
 (defn init []
       (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
