9de3c520da5bd6c2d412a33cdda05dcfd580622e
Wayne Dyck <wayne@imaginedreal.com>
2018-09-27 14:24:57 -0700

    Update Android 'Hello World' example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -9,26 +9,9 @@
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn- convert-to-array [vec]
-  "Vector -> JavaScript Array
-  consumes a Vector and produces an array, it is needed for
-  the list-view, because it only accepts an real JavaScript Array."
-  (let [arr #js[]] (doseq [x vec] (.push arr x)) arr))
-
-(def ds (new data-source #js{:rowHasChanged #(not= %1 %2)}))
-
-(def state (.cloneWithRows ds ;; we are using global state
-             (convert-to-array ["John" "Joel" "James" "Jimmy" "Jackson"
-                                "Jillian" "Julie" "Devin"])))
-
-(defn list-view-basics []
-  [view {:style {:padding-top 22}}
-    [list-view {:dataSource state ;for whatever reason :data-source doesn't work
-                ;; Turn the vector into a React element
-                :render-row #(r/as-element [text %])}]])
-
 (defn app-root []
-  [list-view-basics])
+  [view
+   [text "Hello world!"]])
 
 (defn init []
-      (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
+  (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
