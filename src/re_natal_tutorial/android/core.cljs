384b783ddfeccdfc10991aad265ec0fac0ce8d77
Wayne Dyck <wayne@imaginedreal.com>
2018-09-27 14:24:57 -0700

    Update Android 'Hello World' example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -7,20 +7,9 @@
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(def styles {:bigblue {:color :blue
-                       :fontWeight :bold ; clojure style :font-weight did not work
-                       :fontSize 30}     ; clojure style :font-size did not work
-             :red     {:color :red}})
-
-(defn lots-of-styles []
-  [view
-    [text {:style (:red styles)} "just red"]
-    [text {:style (:bigblue styles)} "just bigblue"]
-    [text {:style [(:bigblue styles) (:red styles)]} "bigblue, then red"]
-    [text {:style [(:red styles) (:bigblue styles)]} "red, then bigblue"]])
-
 (defn app-root []
-  [lots-of-styles])
+  [view
+   [text "Hello world!"]])
 
 (defn init []
-      (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
+  (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
