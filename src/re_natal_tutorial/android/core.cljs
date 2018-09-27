c135ce7a47dd16a1f21e5a981dc01f7ca1970efc
Wayne Dyck <wayne@imaginedreal.com>
2018-09-27 14:24:57 -0700

    Update Android 'Hello World' example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -6,14 +6,9 @@
 (def app-registry (.-AppRegistry ReactNative))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn flex-dimensions-basics []
-  [view {:style {:flex 1}}
-    [view {:style {:flex 1 :background-color :powderblue}}]
-    [view {:style {:flex 2 :background-color :skyblue}}]
-    [view {:style {:flex 3 :background-color :steelblue}}]])
-
 (defn app-root []
-  [flex-dimensions-basics])
+  [view
+   [text "Hello world!"]])
 
 (defn init []
-      (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
+  (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
