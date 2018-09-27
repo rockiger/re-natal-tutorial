a0ea5855bc13de00ad2bacdb4657706e93f8c93e
Wayne Dyck <wayne@imaginedreal.com>
2018-09-27 14:24:57 -0700

    Update Android 'Hello World' example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -9,45 +9,9 @@
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(def state (r/atom "")) ;; we are using global state
-(def favicon (js/require "./images/cljs.png"))
-
-(defn i-scrolled-down []
-  [scroll-view
-    [text {:style {:font-size 96}} "Scroll me plz"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "If you like"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Scrolling down"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "What's the best"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Framework around"]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [image {:source favicon}]
-    [text {:style {:font-size 96}} "Re-Natal"]])
-
 (defn app-root []
-  [i-scrolled-down])
+  [view
+   [text "Hello world!"]])
 
 (defn init []
-      (.registerComponent app-registry "Re-Natal Tutorial" #(r/reactify-component app-root)))
+  (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
