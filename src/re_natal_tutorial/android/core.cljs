45ba59653f067fb616a470df7cc33dab7485d506
Wayne Dyck <wayne@imaginedreal.com>
2018-09-30 23:34:32 -0700

    Update Android 'Height and Width' flex dimensions example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -7,10 +7,10 @@
 (def view (r/adapt-react-class (.-View ReactNative)))
 
 (defn app-root []
-  [view
-   [view {:style {:width 50 :height 50 :background-color :powderblue}}]
-   [view {:style {:width 100 :height 100 :background-color :skyblue}}]
-   [view {:style {:width 150 :height 150 :background-color :steelblue}}]])
+  [view {:style {:flex 1}}
+   [view {:style {:flex 1 :background-color :powderblue}}]
+   [view {:style {:flex 2 :background-color :skyblue}}]
+   [view {:style {:flex 3 :background-color :steelblue}}]])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
