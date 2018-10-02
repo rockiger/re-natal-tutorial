a2e5c0251e6076e56c7fadf734a301c144e9fc8a
Wayne Dyck <wayne@imaginedreal.com>
2018-10-01 22:16:54 -0700

    Update Android 'Layout with Flexbox' align items example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -6,14 +6,21 @@
 (def app-registry (.-AppRegistry ReactNative))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn justify-content-basics []
-  [view {:style {:flex 1 :flex-direction :column :justify-content :space-between}}
+(defn align-items-basics []
+  ;; Try setting ':align-items' to ':flex-start'
+  ;; Try setting ':justify-content' to ':flex-end'
+  ;; Try setting ':flex-direction' to ':row'
+  [view {:style
+         {:flex 1
+          :flex-direction :column
+          :justify-content :center
+          :align-items :center}}
    [view {:style {:width 50 :height 50 :background-color :powderblue}}]
    [view {:style {:width 50 :height 50 :background-color :skyblue}}]
    [view {:style {:width 50 :height 50 :background-color :steelblue}}]])
 
 (defn app-root []
-  [justify-content-basics])
+  [align-items-basics])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
