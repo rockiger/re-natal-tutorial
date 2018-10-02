90bc5cd1108e12569c5177ae49a9c9fdb8c2be6d
Wayne Dyck <wayne@imaginedreal.com>
2018-10-01 23:45:08 -0700

    Add fixed-dimensions-basics function to mirror React-Native tutorial

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -6,11 +6,14 @@
 (def app-registry (.-AppRegistry ReactNative))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn app-root []
+(defn fixed-dimensions-basics []
   [view
    [view {:style {:width 50 :height 50 :background-color :powderblue}}]
    [view {:style {:width 100 :height 100 :background-color :skyblue}}]
    [view {:style {:width 150 :height 150 :background-color :steelblue}}]])
+
+(defn app-root []
+  [fixed-dimensions-basics])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
