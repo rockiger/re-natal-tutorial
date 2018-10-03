6a594236a0e36d49a24c7ab59c4141d9a5835a54
Wayne Dyck <wayne@imaginedreal.com>
2018-10-02 21:39:30 -0700

    Add iOS 'Handling Text Input' pizza translator example

--- src/re_natal_tutorial/ios/core.cljs
+++ src/re_natal_tutorial/ios/core.cljs
@@ -4,23 +4,23 @@
 (def ReactNative (js/require "react-native"))
 
 (def app-registry (.-AppRegistry ReactNative))
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def text-input (r/adapt-react-class (.-TextInput ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn align-items-basics []
-  ;; Try setting ':align-items' to ':flex-start'
-  ;; Try setting ':justify-content' to ':flex-end'
-  ;; Try setting ':flex-direction' to ':row'
-  [view {:style
-         {:flex 1
-          :flex-direction :column
-          :justify-content :center
-          :align-items :center}}
-   [view {:style {:width 50 :height 50 :background-color :powderblue}}]
-   [view {:style {:width 50 :height 50 :background-color :skyblue}}]
-   [view {:style {:width 50 :height 50 :background-color :steelblue}}]])
+(def state (r/atom "")) ;; we are using global state
+
+(defn pizza-translater []
+  [view {:style {:padding 10}}
+   [text-input {:style {:height 40}
+                :placeholder "Type here to translate!"
+                :on-change-text #(reset! state %)}]
+   [text {:style {:padding 10 :font-size 42}}
+    ;; clojure.string does not work in
+    (.join (.map (.split @state " ") #(when (not-empty %) "🍕")) " ")]])
 
 (defn app-root []
-  [align-items-basics])
+  [pizza-translater])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
