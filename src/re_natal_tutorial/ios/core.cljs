3af332805cdfc35cc5cff7718fbef02fa306ef8e
Wayne Dyck <wayne@imaginedreal.com>
2018-10-03 21:49:25 -0700

    Add iOS 'Using a ScrollView' example

--- src/re_natal_tutorial/ios/core.cljs
+++ src/re_natal_tutorial/ios/core.cljs
@@ -4,23 +4,47 @@
 (def ReactNative (js/require "react-native"))
 
 (def app-registry (.-AppRegistry ReactNative))
+(def scroll-view (r/adapt-react-class (.-ScrollView ReactNative)))
+(def image (r/adapt-react-class (.-Image ReactNative)))
 (def text (r/adapt-react-class (.-Text ReactNative)))
-(def text-input (r/adapt-react-class (.-TextInput ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(def state (r/atom "")) ;; we are using global state
-
-(defn pizza-translater []
-  [view {:style {:padding 10}}
-   [text-input {:style {:height 40}
-                :placeholder "Type here to translate!"
-                :on-change-text #(reset! state %)}]
-   [text {:style {:padding 10 :font-size 42}}
-    ;; clojure.string does not work in
-    (.join (.map (.split @state " ") #(when (not-empty %) "🍕")) " ")]])
+(defn i-scrolled-down []
+  [scroll-view
+   [text {:style {:font-size 96}} "Scroll me plz"]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [text {:style {:font-size 96}} "If you like"]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [text {:style {:font-size 96}} "Scrolling down"]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [text {:style {:font-size 96}} "What's the best"]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [text {:style {:font-size 96}} "Framework around"]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
+   [text {:style {:font-size 96}} "Re-Natal"]])
 
 (defn app-root []
-  [pizza-translater])
+  [i-scrolled-down])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
