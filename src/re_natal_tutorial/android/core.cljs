b1ad735a97a4889aef4fb9e21b6b20a4608518cf
Wayne Dyck <wayne@imaginedreal.com>
2018-10-04 23:30:19 -0700

    Update Android 'Using List Views' with FlatList example

--- src/re_natal_tutorial/android/core.cljs
+++ src/re_natal_tutorial/android/core.cljs
@@ -4,47 +4,25 @@
 (def ReactNative (js/require "react-native"))
 
 (def app-registry (.-AppRegistry ReactNative))
-(def scroll-view (r/adapt-react-class (.-ScrollView ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
+(def flat-list (r/adapt-react-class (.-FlatList ReactNative)))
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 
-(defn i-scrolled-down []
-  [scroll-view
-   [text {:style {:font-size 96}} "Scroll me plz"]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [text {:style {:font-size 96}} "If you like"]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [text {:style {:font-size 96}} "Scrolling down"]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [text {:style {:font-size 96}} "What's the best"]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [text {:style {:font-size 96}} "Framework around"]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [image {:source {:uri "https://facebook.github.io/react-native/img/favicon.png"} :style {:width 64 :height 64}}]
-   [text {:style {:font-size 96}} "Re-Natal"]])
+(def styles {:container {:flex 1
+                         :padding-top 22}
+             :item {:padding 10
+                    :fontSize 18
+                    :height 44}})
+
+(defn flat-list-basics []
+  [view {:style (:container styles)}
+   [flat-list {:data #js["Devin" "Jackson" "James" "Joel" "John" "Jillian" "Jimmy" "Julie"]
+               :keyExtractor #(js* "Math.random().toString(32).slice(2)")
+               :render-item #(r/as-element [text {:style (:item styles)}
+                                            (get-in (js->clj % :keywordize-keys true) [:item])])}]])
 
 (defn app-root []
-  [i-scrolled-down])
+  [flat-list-basics])
 
 (defn init []
   (.registerComponent app-registry "ReNatalTutorial" #(r/reactify-component app-root)))
